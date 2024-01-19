242711c3973844ba4955865ad0e3ced745358795
Даниил ᅠ <daniag2001@yandex.ru>
2024-01-19 15:59:05 +0300

    Добавьте файлы проекта.

---
+++ Lab1_Gachkovsky/View/WindowPerson.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Lab1_Gachkovsky.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab1_Gachkovsky.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowPerson.xaml
+    /// </summary>
+    public partial class WindowPerson : Window
+    {
+        public WindowPerson()
+        {
+            InitializeComponent();
+            PersonViewModel vmPerson = new PersonViewModel();
+            ListPerson.ItemsSource = vmPerson.ListPerson;
+        }
+    }
+}
